7a953914d9a17dd41171e83f58044ea5ad076b82
John Edisson Ortiz <j.ortiz3109@gmail.com>
2017-02-21 01:05:34 -0500

    Builder now tries to detect correct value to link content like name, username, email or full_name
    Signed-off-by: John Edisson Ortiz <j.ortiz3109@gmail.com>

--- src/BreadcrumbsBuilder.php
+++ src/BreadcrumbsBuilder.php
@@ -52,6 +52,12 @@ class BreadcrumbsBuilder
     public static $defaultView = 'breadcrumbs::default';
 
     /**
+     * Array of attributes to inspect to content links
+     * @var Array
+     */
+    private $attributes = ['name', 'email', 'full_name', 'username'];
+
+    /**
      * Class constructor
      * @param View    $view View Factory
      * @param Array   $routes Aplication registered GET routes
@@ -65,108 +71,6 @@ class BreadcrumbsBuilder
         $this->segments = explode('/', $request->route()->uri());
         $this->params = $request->route()->parameters();
         $this->lang = $lang;
-    }
-
-    /**
-     * Return route name for the provided path
-     * @param  Array $path path to inspect
-     * @return String|NULL
-     */
-    private function getRouteName(Array $path)
-    {
-        $path = implode('/', $path);
-        foreach ($this->routes as $route) {
-            if ($path === $route->uri()) {
-                return !empty($route->getName())
-                    ? $route->getName()
-                    : $this->routeDotedName($path);
-            }
-        }
-    }
-
-    /**
-     * Return route uri for the provided path
-     * @param  Array $path path to inspect
-     * @return String|NULL
-     */
-    private function getRouteUri(Array $path)
-    {
-        $path = implode('/', $path);
-        foreach ($this->routes as $key => $route) {
-            if ( $path === $route->uri() ) {
-                $uri = $route->uri();
-
-                if (!empty($route->parameterNames())) {
-                    $uri = $this->bindRouteParams($route);
-                }
-
-                //Delete the route thats matched from routes array
-                unset($this->routes[$key]);
-                return $uri;
-            }
-        }
-        return NULL;
-    }
-
-    /**
-     * Return content of the breadcrumb link
-     * @param  String $route_name name of route segment
-     * @return String
-     */
-    private function getLinkContent($route_name)
-    {
-        $field = 'breadcrumbs::links.'.$route_name;
-
-        $content = $this->lang->get($field);
-
-        return $content === $field
-            ? $route_name
-            : $content;
-
-    }
-
-    /**
-     * Render breadcrumb
-     * @return Illuminate\Support\HtmlString
-     * The default template could be published (into resources/views/breadcrumbs/)
-     * or be located inside the components directory (vendor/johndev/breadcrumbs/templates/)
-     */
-    public function render($view = null)
-    {
-
-        $links = array();
-        $route = array();
-
-        foreach ($this->segments as $segment) {
-            $route[] = $segment;
-            $route_name = $this->getRouteName($route);
-
-            if (!empty($route_name)) {
-                $links[] = (object) [
-                    'uri' => $this->getRouteUri($route),
-                    'body'  => $this->getLinkContent($route_name),
-                ];
-            }
-        }
-
-        return new HtmlString($this->view->make($view ?: static::$defaultView, compact('links'))->render());
-    }
-
-    /**
-     * return the route name in doted format
-     * @param  String $path Route path to translate
-     * @return String translated path route to dots
-     */
-    private function routeDotedName($path)
-    {
-
-        $path = str_replace('/', '.', $path);
-
-        // Replace route params in route uri
-        $path = preg_replace("/(\{[a-zA-Z0-9]*\}.?)/", '', $path);
-        $path = preg_replace("/(\{[a-zA-Z0-9]*\})/", '', $path);
-
-        return $path;
     }
 
     /**
@@ -189,6 +93,46 @@ class BreadcrumbsBuilder
     }
 
     /**
+     * Return content of the breadcrumb link
+     * @param  Array $path path to inspect
+     * @return String
+     */
+    private function getLinkContent(Array $paths)
+    {
+        $route_name = $this->getRouteName($paths);
+        $field = 'breadcrumbs::links.'.$route_name;
+        $path = implode('/', $paths);
+
+        $content = $this->lang->get($field);
+
+        foreach ($this->routes as $key => $route) {
+            if ( $path === $route->uri() ) {
+                if (count($route->parameterNames()) === 1) {
+                    $param = $route->parameterNames()[0];
+                    $value = $this->params[$param];
+
+                    if ($value instanceof Model) {
+                        foreach ($this->attributes as $attribute) {
+                            if (!empty($value->$attribute)) {
+                                $content = $this->lang->choice($field, 2, ['name' => $value->$attribute] );
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                //Delete route from routes array
+                unset($this->routes[$key]);
+            }
+        }
+
+        return $content === $field
+            ? $route_name
+            : $content;
+
+    }
+
+    /**
      * Return the value of the param supplied
      * @param  String $param Param name to check
      * @return mixed|null        The param's value
@@ -208,5 +152,105 @@ class BreadcrumbsBuilder
 
         return null;
 
+    }
+
+    /**
+     * Return route name for the provided path
+     * @param  Array $path path to inspect
+     * @return String|NULL
+     */
+    private function getRouteName(Array $path)
+    {
+        $path = implode('/', $path);
+        foreach ($this->routes as $route) {
+            if ($path === $route->uri()) {
+                return !empty($route->getName())
+                    ? $route->getName()
+                    : $this->getRouteDotedName($path);
+            }
+        }
+    }
+
+    /**
+     * Return route uri for the provided path
+     * @param  Array $path path to inspect
+     * @return String|NULL
+     */
+    private function getRouteUri(Array $path)
+    {
+        $path = implode('/', $path);
+        foreach ($this->routes as $key => $route) {
+            if ( $path === $route->uri() ) {
+                $uri = $route->uri();
+
+                if (!empty($route->parameterNames())) {
+                    $uri = $this->bindRouteParams($route);
+                }
+
+                return $uri;
+            }
+        }
+        return NULL;
+    }
+
+    /**
+     * return the route name in doted format
+     * @param  String $path Route path to translate
+     * @return String translated path route to dots
+     */
+    private function getRouteDotedName($path)
+    {
+
+        $path = str_replace('/', '.', $path);
+
+        // Replace route params in route uri
+        $path = preg_replace("/(\{[a-zA-Z0-9]*\}.?)/", '', $path);
+        $path = preg_replace("/(\{[a-zA-Z0-9]*\})/", '', $path);
+
+        return $path;
+    }
+
+    /**
+     * Check if a path is a route
+     * @param  Array $paths paths to inspect
+     * @return boolean
+     */
+    private function isRoute($paths)
+    {
+        $path = implode('/', $paths);
+
+        foreach ($this->routes as $key => $route) {
+            if ( $path === $route->uri() ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Render breadcrumb
+     * @return Illuminate\Support\HtmlString
+     * The default template could be published (into resources/views/breadcrumbs/)
+     * or be located inside the components directory (vendor/johndev/breadcrumbs/templates/)
+     */
+    public function render($view = null)
+    {
+
+        $links = array();
+        $paths = array();
+
+        foreach ($this->segments as $segment) {
+            $paths[] = $segment;
+
+            if ($this->isRoute($paths)) {
+                $links[] = (object) [
+                    'uri' => $this->getRouteUri($paths),
+                    'body'  => $this->getLinkContent($paths),
+                ];
+            }
+        }
+
+        return new HtmlString($this->view->make($view ?: static::$defaultView, compact('links'))->render());
     }
 }
